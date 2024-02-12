4b06409bc13b0f5a270275cfc73b26c2272d90c3
deereaper <131974208+deereaper@users.noreply.github.com>
2024-02-12 12:18:53 +0300

    Update demo.py
    added brackets

--- demo.py
+++ demo.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 data = {
-    "name" : "Issa", "Billy", "Musa"
-    "age" : 21, 19, 23
+    "name" : ["Issa", "Billy", "Musa"]
+    "age" : [21, 19, 23]
        }
 df = pd.Dataframe(data)
 print(data)
